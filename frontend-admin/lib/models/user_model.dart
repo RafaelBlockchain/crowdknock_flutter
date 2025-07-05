63253d2dbdaee9c51b06218e82eded40696796d5
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-05 12:38:49 -0500

    Create user_model.dart

---
+++ frontend-admin/lib/models/user_model.dart
@@ -0,0 +1,24 @@
+// frontend-admin/lib/models/user_model.dart
+
+class UserModel {
+  final String id;
+  final String name;
+  final String email;
+  final String role;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.role,
+  });
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'].toString(),
+      name: json['name'],
+      email: json['email'],
+      role: json['role'],
+    );
+  }
+}
