1291d82c15e65acebe24a88bdf7ca7b0a2ee312b
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-05 18:26:15 -0500

    Update auth_provider.dart

--- frontend-app/lib/core/providers/auth_provider.dart
+++ frontend-app/lib/core/providers/auth_provider.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:frontend_app/core/models/auth_user.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class AuthProvider extends ChangeNotifier {
   final _storage = const FlutterSecureStorage();
@@ -12,6 +15,27 @@ class AuthProvider extends ChangeNotifier {
     _token = await _storage.read(key: 'jwt_token');
     notifyListeners();
   }
+
+  AuthUser? _currentUser;
+AuthUser? get currentUser => _currentUser;
+
+Future<void> fetchCurrentUser() async {
+  if (_token == null) return;
+
+  final res = await http.get(
+    Uri.parse('$baseUrl/auth/me'),
+    headers: {'Authorization': 'Bearer $_token'},
+  );
+
+  if (res.statusCode == 200) {
+    final jsonData = json.decode(res.body);
+    _currentUser = AuthUser.fromJson(jsonData);
+    notifyListeners();
+  } else {
+    // Si falla, cerrar sesión
+    await logout();
+  }
+}
 
   Future<void> login(String token) async {
     _token = token;
