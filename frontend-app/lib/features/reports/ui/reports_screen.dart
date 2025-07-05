c97f29745a0edbe31aed1ce3f2bfb0004fe83d45
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-05 11:48:25 -0500

    Update reports_screen.dart

--- frontend-app/lib/features/reports/ui/reports_screen.dart
+++ frontend-app/lib/features/reports/ui/reports_screen.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_app/features/reports/widgets/report_table.dart';
+
+class ReportsScreen extends StatelessWidget {
+  const ReportsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Reportes del Sistema')),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: ReportTable(),
+      ),
+    );
+  }
+}
 
