85439d2c664f111ff59f09e0882c686524ca345f
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-05 12:12:32 -0500

    Update sessions_chart.dart

--- frontend-app/lib/widgets/charts/sessions_chart.dart
+++ frontend-app/lib/widgets/charts/sessions_chart.dart
@@ -1,1 +1,39 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class SessionsChart extends StatelessWidget {
+  const SessionsChart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: LineChart(
+          LineChartData(
+            titlesData: FlTitlesData(show: true),
+            lineBarsData: [
+              LineChartBarData(
+                spots: [
+                  FlSpot(0, 5),
+                  FlSpot(1, 6),
+                  FlSpot(2, 7),
+                  FlSpot(3, 5),
+                  FlSpot(4, 8),
+                  FlSpot(5, 6),
+                  FlSpot(6, 9),
+                ],
+                isCurved: true,
+                color: Colors.green,
+                barWidth: 3,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
