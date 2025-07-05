4c4d2a13ed64cfeca836cf6aa56a826bda1e35d2
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-05 12:17:47 -0500

    Update user_retention_chart.dart

--- frontend-app/lib/widgets/charts/user_retention_chart.dart
+++ frontend-app/lib/widgets/charts/user_retention_chart.dart
@@ -1,1 +1,39 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class UserRetentionChart extends StatelessWidget {
+  const UserRetentionChart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: LineChart(
+          LineChartData(
+            titlesData: FlTitlesData(show: true),
+            lineBarsData: [
+              LineChartBarData(
+                spots: [
+                  FlSpot(0, 100),
+                  FlSpot(1, 80),
+                  FlSpot(2, 60),
+                  FlSpot(3, 45),
+                  FlSpot(4, 30),
+                  FlSpot(5, 20),
+                  FlSpot(6, 10),
+                ],
+                isCurved: true,
+                color: Colors.purple,
+                barWidth: 3,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
