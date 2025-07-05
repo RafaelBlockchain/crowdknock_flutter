9884f339f694522fa01743e1c26cbb70245e01cc
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-05 14:05:55 -0500

    Update main.dart

--- frontend-app/lib/main.dart
+++ frontend-app/lib/main.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: '.env');
-  runApp(MyApp());
+
+  // Cargar variables de entorno (.env)
+  await dotenv.load(fileName: ".env");
+
+  runApp(const MyApp());
 }
