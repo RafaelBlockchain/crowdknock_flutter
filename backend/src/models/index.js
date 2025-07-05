df64031095940deae2d2c1ab623a29716e85be28
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-05 06:43:24 -0500

    Create index.js

---
+++ backend/src/models/index.js
@@ -0,0 +1,24 @@
+const { sequelize } = require('../config/db');
+
+// Importar todos los modelos
+const User = require('./User');
+const Content = require('./Content');
+const Challenge = require('./Challenge');
+const Report = require('./Report');
+const Payment = require('./Payment');
+const Setting = require('./Setting');
+const Metric = require('./Metric'); // opcional
+
+// Las relaciones ya están definidas dentro de cada modelo individualmente.
+
+// Exportar todo junto
+module.exports = {
+  sequelize,
+  User,
+  Content,
+  Challenge,
+  Report,
+  Payment,
+  Setting,
+  Metric,
+};
