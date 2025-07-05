19cb37ab8658bbe652bee5582d4e6bd20800b87b
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-05 10:19:35 -0500

    Update main.dart

--- frontend-app/lib/main.dart
+++ frontend-app/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const CrowdKnockApp());
+  await dotenv.load(fileName: '.env');
+  runApp(MyApp());
 }
-
