92a453b05e01df6526ec7cc6309c0f5749f1837b
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-05 10:15:36 -0500

    Update auth_service.dart

--- frontend-app/lib/core/services/auth_service.dart
+++ frontend-app/lib/core/services/auth_service.dart
@@ -1,1 +1,29 @@
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  static Future<bool> login(String email, String password) async {
+    final baseUrl = dotenv.env['API_BASE_URL'];
+    final url = Uri.parse('$baseUrl/auth/login');
+
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'email': email, 'password': password}),
+    );
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      final token = json['token'];
+
+      // Aquí puedes guardar el token en SharedPreferences u otro método
+      print('Token recibido: $token');
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
 
