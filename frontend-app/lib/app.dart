a8be4e1e14974c770e96f2d94a415d9fd61e86e8
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-05 08:26:44 -0500

    Update app.dart

--- frontend-app/lib/app.dart
+++ frontend-app/lib/app.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'routes/app_router.dart';
+
+class CrowdKnockApp extends StatefulWidget {
+  const CrowdKnockApp({super.key});
+
+  @override
+  State<CrowdKnockApp> createState() => _CrowdKnockAppState();
+}
+
+class _CrowdKnockAppState extends State<CrowdKnockApp> {
+  @override
+  void initState() {
+    super.initState();
+    _loadEnv();
+  }
+
+  Future<void> _loadEnv() async {
+    await dotenv.load(fileName: ".env");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'CrowdKnock',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.indigo,
+      ),
+      routerConfig: AppRouter.router,
+    );
+  }
+}
 
