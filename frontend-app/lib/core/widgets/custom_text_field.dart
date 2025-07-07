e0df4752e96685ec14f56b9328c23a89a5246d7e
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-07 17:57:53 -0500

    Update custom_text_field.dart

--- frontend-app/lib/core/widgets/custom_text_field.dart
+++ frontend-app/lib/core/widgets/custom_text_field.dart
@@ -2,32 +2,46 @@ import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
-  final String labelText;
+  final String label;
   final bool obscureText;
-  final String? Function(String?)? validator;
   final TextInputType keyboardType;
+  final String? Function(String?)? validator;
+  final bool useFormField;
 
   const CustomTextField({
     super.key,
     required this.controller,
-    required this.labelText,
+    required this.label,
     this.obscureText = false,
-    this.validator,
     this.keyboardType = TextInputType.text,
+    this.validator,
+    this.useFormField = true,
   });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      obscureText: obscureText,
-      validator: validator,
-      keyboardType: keyboardType,
-      decoration: InputDecoration(
-        labelText: labelText,
-        border: const OutlineInputBorder(),
-        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-      ),
+    final inputDecoration = InputDecoration(
+      labelText: label,
+      border: const OutlineInputBorder(),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+    );
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: useFormField
+          ? TextFormField(
+              controller: controller,
+              obscureText: obscureText,
+              keyboardType: keyboardType,
+              validator: validator,
+              decoration: inputDecoration,
+            )
+          : TextField(
+              controller: controller,
+              obscureText: obscureText,
+              keyboardType: keyboardType,
+              decoration: inputDecoration,
+            ),
     );
   }
 }
