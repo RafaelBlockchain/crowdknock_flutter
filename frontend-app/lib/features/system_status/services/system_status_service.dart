d0b5e813ac7f45e8c40f3769b2e5ac40269813b0
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-07 05:28:32 -0500

    Create system_status_service.dart

---
+++ frontend-app/lib/features/system_status/services/system_status_service.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:frontend_app/core/constants/env.dart';
+import 'package:frontend_app/features/system_status/models/system_status_model.dart';
+
+class SystemStatusService {
+  Future<SystemStatus> fetchStatus() async {
+    final url = Uri.parse('$baseUrl/system/status');
+    final res = await http.get(url, headers: {
+      'Authorization': 'Bearer $jwtToken', // usa tu método JWT
+    });
+
+    if (res.statusCode == 200) {
+      return SystemStatus.fromJson(json.decode(res.body));
+    } else {
+      throw Exception('Error al obtener estado del sistema');
+    }
+  }
+}
