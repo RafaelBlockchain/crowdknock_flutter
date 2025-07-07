5ae7719d73cc9eff355eeccc298ff585496c06a8
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-07 05:57:24 -0500

    Update system_status_service.dart

--- frontend-app/lib/features/system_status/services/system_status_service.dart
+++ frontend-app/lib/features/system_status/services/system_status_service.dart
@@ -2,9 +2,12 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:frontend_app/core/constants/env.dart';
 import 'package:frontend_app/features/system_status/models/system_status_model.dart';
+import '../models/system_status_model.dart';
+import '../../../core/constants/env.dart';
 
 class SystemStatusService {
   Future<SystemStatus> fetchStatus() async {
+    final response = await http.get(Uri.parse('$baseUrl/system/status'));
     final url = Uri.parse('$baseUrl/system/status');
     final res = await http.get(url, headers: {
       'Authorization': 'Bearer $jwtToken', // usa tu método JWT
