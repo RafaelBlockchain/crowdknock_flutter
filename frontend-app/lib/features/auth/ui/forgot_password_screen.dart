a04446b2d08e39f7353a38fed5ae78ba88b4af61
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-07 19:15:11 -0500

    Create forgot_password_screen.dart

---
+++ frontend-app/lib/features/auth/ui/forgot_password_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_app/core/widgets/custom_text_field.dart';
+import 'package:frontend_app/core/widgets/primary_button.dart';
+
+class ForgotPasswordScreen extends StatefulWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  bool _loading = false;
+
+  void _submit() async {
+    if (!_formKey.currentState!.validate()) return;
+
+    setState(() => _loading = true);
+
+    try {
+      // Simular envío al backend (POST /auth/forgot-password)
+      await Future.delayed(const Duration(seconds: 2)); // Simula solicitud
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Revisa tu correo para instrucciones')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Error al enviar la solicitud')),
+      );
+    } finally {
+      setState(() => _loading = false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Recuperar contraseña')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              CustomTextField(
+                label: 'Correo electrónico',
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) =>
+                    value!.contains('@') ? null : 'Email inválido',
+              ),
+              const SizedBox(height: 30),
+              PrimaryButton(
+                text: _loading ? 'Enviando...' : 'Enviar instrucciones',
+                onPressed: _loading ? null : _submit,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
