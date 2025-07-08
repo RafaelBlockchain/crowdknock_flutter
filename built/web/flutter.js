9850ef4df637f2ae4dbb1b9e60a23743b3a27e32
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-08 18:48:40 -0500

    Update flutter.js

--- built/web/flutter.js
+++ built/web/flutter.js
@@ -1,1 +1,9 @@
+// web/flutter.js
+
+(function() {
+  let script = document.createElement('script');
+  script.src = 'main.dart.js';
+  script.defer = true;
+  document.body.appendChild(script);
+})();
 
