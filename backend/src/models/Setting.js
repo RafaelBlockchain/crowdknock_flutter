cffa21d47de5204fd068d3a53f27149632e53fb7
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-09 12:14:51 -0500

    Update Setting.js

--- backend/src/models/Setting.js
+++ backend/src/models/Setting.js
@@ -1,24 +1,32 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-const Setting = sequelize.define('Setting', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
+const Setting = sequelize.define(
+  'Setting',
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    key: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    value: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
   },
-  key: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  value: {
-    type: DataTypes.TEXT,
-    allowNull: false,
+  {
+    tableName: 'settings',
+    timestamps: true,
+    underscored: true,
   }
-}, {
-  tableName: 'settings',
-  timestamps: true,
-});
+);
 
 module.exports = Setting;
