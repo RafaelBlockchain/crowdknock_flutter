4a45aaae6127bf4ffd2c0704d816e165d1b1d39d
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-09 12:10:48 -0500

    Update Metric.js

--- backend/src/models/Metric.js
+++ backend/src/models/Metric.js
@@ -1,28 +1,37 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-const Metric = sequelize.define('Metric', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
+const Metric = sequelize.define(
+  'Metric',
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    value: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    recordedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+      field: 'recorded_at',
+    },
   },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  value: {
-    type: DataTypes.FLOAT,
-    allowNull: false,
-  },
-  recordedAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
+  {
+    tableName: 'metrics',
+    timestamps: false,
+    underscored: true,
   }
-}, {
-  tableName: 'metrics',
-  timestamps: false,
-});
+);
 
 module.exports = Metric;
