94d39e4eab08f3f0a71a984fa9251a2c1e3ddd9e
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 14:59:12 -0500

    Update payment_model.dart

--- frontend-admin/lib/models/payment_model.dart
+++ frontend-admin/lib/models/payment_model.dart
@@ -1,1 +1,24 @@
+class PaymentModel {
+  final String id;
+  final String user;
+  final double amount;
+  final String date;
+  final String status;
+
+  PaymentModel({
+    required this.id,
+    required this.user,
+    required this.amount,
+    required this.date,
+    required this.status,
+  });
+
+  factory PaymentModel.fromJson(Map<String, dynamic> json) => PaymentModel(
+        id: json['id'],
+        user: json['user'],
+        amount: (json['amount'] as num).toDouble(),
+        date: json['date'],
+        status: json['status'],
+      );
+}
 
