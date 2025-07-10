790f8718348afb6d1d3094d3327299d5554cadf0
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 09:57:37 -0500

    Update constants.dart

--- frontend-app/lib/config/constants.dart
+++ frontend-app/lib/config/constants.dart
@@ -1,1 +1,8 @@
+class AppConstants {
+  static const double defaultPadding = 16.0;
+  static const double cardRadius = 12.0;
+  static const int maxFileUploadSizeMb = 10;
+
+  static const List<String> supportedLanguages = ['es', 'en'];
+}
 
