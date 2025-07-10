4d3ab0402284b39f217e6688948ba0ba5c2d90a9
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 07:12:15 -0500

    Update challenge_service.dart

--- frontend-app/lib/features/challenges/services/challenge_service.dart
+++ frontend-app/lib/features/challenges/services/challenge_service.dart
@@ -5,11 +5,22 @@ class ChallengeService {
   final ApiService _api = ApiService();
 
   Future<Challenge> getChallengeDetail(String id) async {
-    final response = await _api.get('/challenges/\$id');
+    final response = await _api.get('/challenges/$id');
     if (response.statusCode == 200) {
       return Challenge.fromJson(response.data);
     } else {
       throw Exception(response.data['message']);
+    }
+  }
+
+  Future<List<Challenge>> getAllChallenges() async {
+    final response = await _api.get('/challenges');
+    if (response.statusCode == 200 && response.data is List) {
+      return (response.data as List)
+          .map((item) => Challenge.fromJson(item))
+          .toList();
+    } else {
+      throw Exception('Error al obtener la lista de retos');
     }
   }
 }
