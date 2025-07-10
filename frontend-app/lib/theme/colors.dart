82ae8f1862a8c52df1c88ed942f762c525ad8a1b
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 09:42:04 -0500

    Update colors.dart

--- frontend-app/lib/theme/colors.dart
+++ frontend-app/lib/theme/colors.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF003366);     // Azul oscuro
+  static const accent = Color(0xFF00BFA6);      // Verde Aqua
+  static const background = Color(0xFFF9F9F9);  // Fondo claro
+  static const text = Color(0xFF222222);        // Texto principal
+  static const gray = Color(0xFF888888);        // Texto secundario
+  static const danger = Color(0xFFE53935);      // Rojo
+  static const success = Color(0xFF43A047);     // Verde éxito
+}
 
