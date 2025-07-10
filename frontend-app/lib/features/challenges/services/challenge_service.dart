670127ebefbdd8c8027082224741f8f3a4b156e2
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 07:40:02 -0500

    Update challenge_service.dart

--- frontend-app/lib/features/challenges/services/challenge_service.dart
+++ frontend-app/lib/features/challenges/services/challenge_service.dart
@@ -1,9 +1,9 @@
-import '../../../core/services/api_service.dart';
-import '../models/challenge.dart';
-import '../models/challenge_submission_request.dart';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
+import '../../../core/services/api_service.dart';
+import '../models/challenge.dart';
+import '../models/challenge_submission_request.dart';
 
 class ChallengeService {
   final ApiService _api = ApiService();
@@ -17,31 +17,6 @@ class ChallengeService {
     }
   }
 
-  Future<void> submitChallengeParticipation(
-    String challengeId, ChallengeSubmissionRequest data) async {
-  final uri = Uri.parse('${_api.baseUrl}/challenges/$challengeId/submissions');
-
-  final request = http.MultipartRequest('POST', uri)
-    ..headers.addAll(await _api.headers)
-    ..fields['comment'] = data.comment;
-
-  if (data.file != null) {
-    final file = await http.MultipartFile.fromPath(
-      'file',
-      data.file!.path,
-      contentType: MediaType('application', 'octet-stream'), // Cambia si solo aceptas imágenes
-    );
-    request.files.add(file);
-  }
-
-  final streamedResponse = await request.send();
-  final response = await http.Response.fromStream(streamedResponse);
-
-  if (response.statusCode != 200) {
-    throw Exception('Error: ${response.body}');
-  }
-}
-  
   Future<List<Challenge>> getAllChallenges() async {
     final response = await _api.get('/challenges');
     if (response.statusCode == 200 && response.data is List) {
@@ -50,6 +25,31 @@ class ChallengeService {
           .toList();
     } else {
       throw Exception('Error al obtener la lista de retos');
+    }
+  }
+
+  Future<void> submitChallengeParticipation(
+    String challengeId, ChallengeSubmissionRequest data) async {
+    final uri = Uri.parse('${_api.baseUrl}/challenges/$challengeId/submissions');
+
+    final request = http.MultipartRequest('POST', uri)
+      ..headers.addAll(await _api.headers)
+      ..fields['comment'] = data.comment;
+
+    if (data.file != null) {
+      final file = await http.MultipartFile.fromPath(
+        'file',
+        data.file!.path,
+        contentType: MediaType('application', 'octet-stream'),
+      );
+      request.files.add(file);
+    }
+
+    final streamedResponse = await request.send();
+    final response = await http.Response.fromStream(streamedResponse);
+
+    if (response.statusCode != 200) {
+      throw Exception('Error: ${response.body}');
     }
   }
 }
