bc9a1437cc1b5b295d469487349d9d872876bb8a
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 10:01:18 -0500

    Update api_interceptor.dart

--- frontend-app/lib/core/middleware/api_interceptor.dart
+++ frontend-app/lib/core/middleware/api_interceptor.dart
@@ -1,1 +1,18 @@
+import 'package:dio/dio.dart';
+
+class ApiInterceptor extends Interceptor {
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    // Puedes manejar errores comunes aquí
+    if (err.response?.statusCode == 401) {
+      // Token inválido → cerrar sesión
+    }
+
+    if (err.type == DioExceptionType.connectionTimeout) {
+      // Mostrar mensaje de red lenta
+    }
+
+    super.onError(err, handler);
+  }
+}
 
