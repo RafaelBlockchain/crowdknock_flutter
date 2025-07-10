29c2947928b6f88a0f76898ed8dad9d14a7afc68
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 07:51:44 -0500

    Create upload.middleware.js

---
+++ backend/src/middlewares/upload.middleware.js
@@ -0,0 +1,17 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'src/uploads/');
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const filename = Date.now() + '-' + Math.round(Math.random() * 1E9) + ext;
+    cb(null, filename);
+  }
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
