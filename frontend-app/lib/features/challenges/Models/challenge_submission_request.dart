ecaa878ffd8b0299643f76e871cf6c058642be4f
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 07:31:04 -0500

    Create challenge_submission_request.dart

---
+++ frontend-app/lib/features/challenges/Models/challenge_submission_request.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+class ChallengeSubmissionRequest {
+  final String comment;
+  final File? file;
+
+  ChallengeSubmissionRequest({
+    required this.comment,
+    this.file,
+  });
+
+  Map<String, String> toJson() => {
+        'comment': comment,
+      };
+}
