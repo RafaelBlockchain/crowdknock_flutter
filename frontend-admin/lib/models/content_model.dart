5b0ff1a00a9a6aa6de35a6dcb74a031f84f449e3
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 14:58:52 -0500

    Update content_model.dart

--- frontend-admin/lib/models/content_model.dart
+++ frontend-admin/lib/models/content_model.dart
@@ -1,1 +1,24 @@
+class ContentModel {
+  final String id;
+  final String title;
+  final String type;
+  final String author;
+  final String status;
+
+  ContentModel({
+    required this.id,
+    required this.title,
+    required this.type,
+    required this.author,
+    required this.status,
+  });
+
+  factory ContentModel.fromJson(Map<String, dynamic> json) => ContentModel(
+        id: json['id'],
+        title: json['title'],
+        type: json['type'],
+        author: json['author'],
+        status: json['status'],
+      );
+}
 
