531686a0a49fa8cc2c9f3dd3d2d668c48cdaf5e4
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 10:13:14 -0500

    Update error_model.dart

--- frontend-app/lib/core/models/error_model.dart
+++ frontend-app/lib/core/models/error_model.dart
@@ -1,1 +1,17 @@
+class ErrorModel {
+  final String message;
+  final int? statusCode;
+
+  ErrorModel({
+    required this.message,
+    this.statusCode,
+  });
+
+  factory ErrorModel.fromJson(Map<String, dynamic> json) {
+    return ErrorModel(
+      message: json['message'] ?? 'Error desconocido',
+      statusCode: json['statusCode'],
+    );
+  }
+}
 
