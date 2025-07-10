55e0b1faa333b6b32b8595956b5a9ecb8d10b8e5
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 09:34:41 -0500

    Update splash_page.dart

--- frontend-app/lib/pages/splash_page.dart
+++ frontend-app/lib/pages/splash_page.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacementNamed('/onboarding');
+    });
+
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
 
