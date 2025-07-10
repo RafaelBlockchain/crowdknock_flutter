23dcab9323bc64fe978a2d9e6ffeb16b29c2ba4d
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 09:53:43 -0500

    Update env.dart

--- frontend-app/lib/config/env.dart
+++ frontend-app/lib/config/env.dart
@@ -1,1 +1,8 @@
+class Env {
+  static const String appName = String.fromEnvironment('APP_NAME', defaultValue: 'CrowdKnock');
+  static const String environment = String.fromEnvironment('ENV', defaultValue: 'development');
+  static const String apiBaseUrl = String.fromEnvironment('API_BASE_URL', defaultValue: 'http://localhost:3000/api');
+
+  static const bool isProduction = environment == 'production';
+}
 
