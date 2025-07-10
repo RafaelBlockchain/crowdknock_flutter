ad0fa8b52475f413f54f7f6e4fc7589459744405
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 07:27:49 -0500

    Update challenge_repository.dart

--- frontend-app/lib/features/challenges/repositories/challenge_repository.dart
+++ frontend-app/lib/features/challenges/repositories/challenge_repository.dart
@@ -1,11 +1,18 @@
 import '../models/challenge.dart';
 import '../services/challenge_service.dart';
+import '../models/challenge_submission_request.dart';
 
 class ChallengeRepository {
   final ChallengeService _service = ChallengeService();
 
   Future<Challenge> fetchChallenge(String id) {
     return _service.getChallengeDetail(id);
+  }
+
+  
+  Future<void> submitParticipation(
+    String challengeId, ChallengeSubmissionRequest submission) {
+    return _service.submitChallengeParticipation(challengeId, submission);
   }
 
   Future<List<Challenge>> fetchAllChallenges() {
