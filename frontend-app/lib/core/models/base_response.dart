1265963a6e8c92aacdd52ed3a41b6d7eb2c96b1d
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 10:12:54 -0500

    Update base_response.dart

--- frontend-app/lib/core/models/base_response.dart
+++ frontend-app/lib/core/models/base_response.dart
@@ -1,1 +1,23 @@
+class BaseResponse<T> {
+  final bool success;
+  final T? data;
+  final String? message;
+
+  BaseResponse({
+    required this.success,
+    this.data,
+    this.message,
+  });
+
+  factory BaseResponse.fromJson(
+    Map<String, dynamic> json,
+    T Function(dynamic) fromJsonT,
+  ) {
+    return BaseResponse<T>(
+      success: json['success'],
+      data: json['data'] != null ? fromJsonT(json['data']) : null,
+      message: json['message'],
+    );
+  }
+}
 
