b32a1dceea1b1e7faec91c01cc9e7a7544bd8495
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 09:35:39 -0500

    Update navigation_controller.dart

--- frontend-app/lib/pages/navigation_controller.dart
+++ frontend-app/lib/pages/navigation_controller.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+import 'splash_page.dart';
+import 'onboarding_page.dart';
+import 'not_found_page.dart';
+
+final Map<String, WidgetBuilder> appRoutes = {
+  '/': (_) => const SplashPage(),
+  '/onboarding': (_) => const OnboardingPage(),
+  '/home': (_) => const HomePage(),
+};
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  final builder = appRoutes[settings.name];
+  return MaterialPageRoute(
+    builder: builder ?? (_) => const NotFoundPage(),
+  );
+}
 
