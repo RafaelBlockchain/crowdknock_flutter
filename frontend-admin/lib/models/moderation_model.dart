a2f30dc0a01e3f5ad80731375a3c241119ae22a7
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 14:59:55 -0500

    Update moderation_model.dart

--- frontend-admin/lib/models/moderation_model.dart
+++ frontend-admin/lib/models/moderation_model.dart
@@ -1,1 +1,21 @@
+class ModerationItem {
+  final String id;
+  final String content;
+  final String reason;
+  final String type; // video, comment, etc.
+
+  ModerationItem({
+    required this.id,
+    required this.content,
+    required this.reason,
+    required this.type,
+  });
+
+  factory ModerationItem.fromJson(Map<String, dynamic> json) => ModerationItem(
+        id: json['id'],
+        content: json['content'],
+        reason: json['reason'],
+        type: json['type'],
+      );
+}
 
