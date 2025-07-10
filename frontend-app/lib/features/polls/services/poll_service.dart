755f1f86033c9d1e98d418f4790c7e4c676b1113
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 09:19:20 -0500

    Update poll_service.dart

--- frontend-app/lib/features/polls/services/poll_service.dart
+++ frontend-app/lib/features/polls/services/poll_service.dart
@@ -1,1 +1,23 @@
+import '../../../core/services/api_service.dart';
+import '../models/poll_model.dart';
+
+class PollService {
+  final ApiService _api = ApiService();
+
+  Future<List<PollModel>> fetchPolls() async {
+    final response = await _api.get('/polls');
+    return (response.data as List)
+        .map((item) => PollModel.fromJson(item))
+        .toList();
+  }
+
+  Future<PollModel> fetchPollById(String id) async {
+    final response = await _api.get('/polls/$id');
+    return PollModel.fromJson(response.data);
+  }
+
+  Future<void> vote(String pollId, String optionId) async {
+    await _api.post('/polls/$pollId/vote', data: {'optionId': optionId});
+  }
+}
 
