275c43fcb1d743ca7a3803eb07069f09b79ce5e8
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 10:13:33 -0500

    Update api_result.dart

--- frontend-app/lib/core/models/api_result.dart
+++ frontend-app/lib/core/models/api_result.dart
@@ -1,1 +1,11 @@
+class ApiResult<T> {
+  final T? data;
+  final String? error;
+
+  ApiResult.success(this.data) : error = null;
+  ApiResult.failure(this.error) : data = null;
+
+  bool get isSuccess => data != null;
+  bool get isFailure => error != null;
+}
 
