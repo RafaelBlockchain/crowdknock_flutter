76af7735ebcdfed065bdf9327f289e04f6db84b9
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 09:05:34 -0500

    Update content_service.dart

--- frontend-app/lib/features/manage_content/services/content_service.dart
+++ frontend-app/lib/features/manage_content/services/content_service.dart
@@ -2,6 +2,26 @@ import 'package:flutter/foundation.dart';
 import '../../../core/services/api_service.dart';
 import '../models/content_model.dart';
 
+const db = require('../../config/db');
+
+async function getAllContent() {
+  const query = 'SELECT * FROM contents ORDER BY created_at DESC';
+  const { rows } = await db.query(query);
+  return rows;
+}
+
+async function getContentById(id) {
+  const query = 'SELECT * FROM contents WHERE id = $1';
+  const { rows } = await db.query(query, [id]);
+  return rows[0];
+}
+
+module.exports = {
+  getAllContent,
+  getContentById,
+};
+
+
 class ContentService {
   final ApiService _api = ApiService();
 
