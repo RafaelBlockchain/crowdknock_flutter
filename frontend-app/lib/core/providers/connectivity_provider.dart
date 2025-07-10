328062c0804ef9aeaf1cfb44c551145877c3238b
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 10:17:38 -0500

    Update connectivity_provider.dart

--- frontend-app/lib/core/providers/connectivity_provider.dart
+++ frontend-app/lib/core/providers/connectivity_provider.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class ConnectivityProvider extends ChangeNotifier {
+  bool _isOnline = true;
+
+  bool get isOnline => _isOnline;
+
+  ConnectivityProvider() {
+    Connectivity().onConnectivityChanged.listen((result) {
+      _isOnline = result != ConnectivityResult.none;
+      notifyListeners();
+    });
+  }
+}
 
