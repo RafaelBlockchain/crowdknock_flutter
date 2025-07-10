ee4477c30af59cff815b032b3047171e4f847930
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 09:35:20 -0500

    Update not_found_page.dart

--- frontend-app/lib/pages/not_found_page.dart
+++ frontend-app/lib/pages/not_found_page.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NotFoundPage extends StatelessWidget {
+  const NotFoundPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Página no encontrada')),
+      body: const Center(
+        child: Text("404 - No se encontró la ruta."),
+      ),
+    );
+  }
+}
 
