7e8934b7358888a4199cb7e85bfc065446756561
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 09:48:55 -0500

    Update line_chart_widget.dart

--- frontend-app/lib/widgets/charts/line_chart_widget.dart
+++ frontend-app/lib/widgets/charts/line_chart_widget.dart
@@ -1,1 +1,36 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class LineChartWidget extends StatelessWidget {
+  final List<double> values;
+
+  const LineChartWidget({super.key, required this.values});
+
+  @override
+  Widget build(BuildContext context) {
+    return LineChart(
+      LineChartData(
+        titlesData: FlTitlesData(
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, _) => Text('${value.toInt()}'),
+            ),
+          ),
+        ),
+        lineBarsData: [
+          LineChartBarData(
+            spots: List.generate(
+              values.length,
+              (index) => FlSpot(index.toDouble(), values[index]),
+            ),
+            isCurved: true,
+            color: Colors.blue,
+            dotData: FlDotData(show: false),
+          )
+        ],
+      ),
+    );
+  }
+}
 
