65443309ac2a2579278a96a092e9acdc6d5ff24f
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 10:33:22 -0500

    Update challenge_provider.dart

--- frontend-app/lib/data/providers/challenge_provider.dart
+++ frontend-app/lib/data/providers/challenge_provider.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
+import '../models/challenge_model.dart';
+import '../../services/challenge_service.dart';
+
+class ChallengeProvider extends ChangeNotifier {
+  List<ChallengeModel> _challenges = [];
+  bool _isLoading = false;
+
+  List<ChallengeModel> get challenges => _challenges;
+  bool get isLoading => _isLoading;
+
+  Future<void> loadChallenges() async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      final data = await ChallengeService().getAllChallenges();
+      _challenges = data;
+    } catch (e) {
+      _challenges = [];
+    }
+
+    _isLoading = false;
+    notifyListeners();
+  }
+}
 
