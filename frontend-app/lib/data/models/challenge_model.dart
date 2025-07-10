edfe696949a88d5a6cf76c462d38dc767c02a77c
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 10:22:55 -0500

    Update challenge_model.dart

--- frontend-app/lib/data/models/challenge_model.dart
+++ frontend-app/lib/data/models/challenge_model.dart
@@ -1,1 +1,34 @@
+class ChallengeModel {
+  final String id;
+  final String title;
+  final String description;
+  final DateTime createdAt;
+  final String author;
+
+  ChallengeModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.createdAt,
+    required this.author,
+  });
+
+  factory ChallengeModel.fromJson(Map<String, dynamic> json) {
+    return ChallengeModel(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      createdAt: DateTime.parse(json['createdAt']),
+      author: json['author'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'description': description,
+    'createdAt': createdAt.toIso8601String(),
+    'author': author,
+  };
+}
 
