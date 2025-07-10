0ef2571e143dd68602be3218bcc242b9f92d256e
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 09:57:17 -0500

    Update api_config.dart

--- frontend-app/lib/config/api_config.dart
+++ frontend-app/lib/config/api_config.dart
@@ -1,1 +1,22 @@
+import 'package:dio/dio.dart';
+import 'env.dart';
+
+class ApiConfig {
+  static final Dio dio = Dio(BaseOptions(
+    baseUrl: Env.apiBaseUrl,
+    connectTimeout: const Duration(seconds: 10),
+    receiveTimeout: const Duration(seconds: 15),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  ));
+
+  static void setAuthToken(String token) {
+    dio.options.headers["Authorization"] = "Bearer $token";
+  }
+
+  static void clearAuthToken() {
+    dio.options.headers.remove("Authorization");
+  }
+}
 
