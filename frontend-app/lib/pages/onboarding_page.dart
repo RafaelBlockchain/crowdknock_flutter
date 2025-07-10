24ef0b11ab6d780c42dc6d0f627075958a9df176
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 09:35:01 -0500

    Update onboarding_page.dart

--- frontend-app/lib/pages/onboarding_page.dart
+++ frontend-app/lib/pages/onboarding_page.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPage extends StatelessWidget {
+  const OnboardingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Bienvenido a CrowdKnock", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            const SizedBox(height: 24),
+            const Text("Tu voz, tu impacto. Participa, vota y transforma."),
+            const SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () => Navigator.pushReplacementNamed(context, '/home'),
+              child: const Text("Empezar"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
 
