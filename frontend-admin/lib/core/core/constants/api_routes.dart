40823d916ce9b40135555c31d717a66eae8ad7ea
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-10 19:07:36 -0500

    Update api_routes.dart

--- frontend-admin/lib/core/core/constants/api_routes.dart
+++ frontend-admin/lib/core/core/constants/api_routes.dart
@@ -1,1 +1,8 @@
+class ApiRoutes {
+  static const String login = '/auth/login';
+  static const String users = '/users';
+  static const String userById = '/users/'; // + {id}
+  static const String content = '/content';
+  static const String metrics = '/metrics';
+}
 
