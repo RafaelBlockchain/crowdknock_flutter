c7bd5d6652aa204721fa98a7e8027d736f9f8cff
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-11 15:22:41 -0500

    Update content_row_actions.dart

--- frontend-admin/lib/features/manage_content/widgets/content_row_actions.dart
+++ frontend-admin/lib/features/manage_content/widgets/content_row_actions.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+import '../../../data/models/content_model.dart';
 
+class ContentRowActions extends StatelessWidget {
+  final ContentModel content;
+  final void Function(ContentModel) onEdit;
+  final void Function(ContentModel) onDelete;
+
+  const ContentRowActions({
+    super.key,
+    required this.content,
+    required this.onEdit,
+    required this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          tooltip: 'Editar',
+          icon: const Icon(Icons.edit, color: Colors.blue),
+          onPressed: () => onEdit(content),
+        ),
+        IconButton(
+          tooltip: 'Eliminar',
+          icon: const Icon(Icons.delete, color: Colors.red),
+          onPressed: () => onDelete(content),
+        ),
+      ],
+    );
+  }
+}
