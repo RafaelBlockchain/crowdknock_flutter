1bbca162f89d8c01e8017c538eeef13ace38f4a6
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-11 12:24:37 -0500

    Update settings_screen.dart

--- frontend-admin/lib/screens/settings_screen.dart
+++ frontend-admin/lib/screens/settings_screen.dart
@@ -1,9 +1,37 @@
 import 'package:flutter/material.dart';
 import '../layout/admin_scaffold.dart';
+import 'package:provider/provider.dart';
+import 'package:frontend_app/core/providers/locale_provider.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
+
+  @override
+  Widget build(BuildContext context) {
+    final provider = Provider.of<LocaleProvider>(context);
+    final currentLocale = provider.locale;
+
+    return DropdownButton<Locale>(
+      value: currentLocale,
+      icon: const Icon(Icons.language),
+      onChanged: (Locale? newLocale) {
+        if (newLocale != null) {
+          provider.setLocale(newLocale);
+        }
+      },
+      items: L10n.all.map((locale) {
+        final flag = L10n.getFlag(locale.languageCode);
+        return DropdownMenuItem(
+          value: locale,
+          child: Text(flag),
+        );
+      }).toList(),
+    );
+  }
+}
+
+  
   @override
   State<SettingsScreen> createState() => _SettingsScreenState();
 }
