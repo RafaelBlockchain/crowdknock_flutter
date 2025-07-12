a55efddaa02108c6cfeb23a02c4ab350b3c77aac
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-12 09:33:48 -0500

    Update form_validators.dart

--- frontend-app/lib/core/utils/form_validators.dart
+++ frontend-app/lib/core/utils/form_validators.dart
@@ -1,16 +1,1 @@
-class FormatUtils {
-  static String capitalize(String text) {
-    if (text.isEmpty) return '';
-    return text[0].toUpperCase() + text.substring(1).toLowerCase();
-  }
-
-  static String truncate(String text, int maxLength) {
-    if (text.length <= maxLength) return text;
-    return '${text.substring(0, maxLength)}...';
-  }
-
-  static String formatNumber(num value, {int decimals = 2}) {
-    return value.toStringAsFixed(decimals);
-  }
-}
 
