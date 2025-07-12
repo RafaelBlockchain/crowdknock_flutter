25f4b32ecb05ceb0cff9a3539871db61884a86de
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-12 09:34:58 -0500

    Update logger.dart

--- frontend-app/lib/core/utils/logger.dart
+++ frontend-app/lib/core/utils/logger.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+class Logger {
+  static void log(String message) {
+    if (kDebugMode) {
+      debugPrint('[LOG] $message');
+    }
+  }
+
+  static void error(String message, [Object? error]) {
+    if (kDebugMode) {
+      debugPrint('[ERROR] $message');
+      if (error != null) debugPrint(error.toString());
+    }
+  }
+}
 
