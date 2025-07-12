92238ad8e5988c4c12bfd057873904ec757227ef
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-12 09:35:16 -0500

    Update navigation_helper.dart

--- frontend-app/lib/core/utils/navigation_helper.dart
+++ frontend-app/lib/core/utils/navigation_helper.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NavigationHelper {
+  static void goTo(BuildContext context, String route) {
+    Navigator.pushNamed(context, route);
+  }
+
+  static void goBack(BuildContext context) {
+    Navigator.pop(context);
+  }
+
+  static void goReplace(BuildContext context, String route) {
+    Navigator.pushReplacementNamed(context, route);
+  }
+}
 
