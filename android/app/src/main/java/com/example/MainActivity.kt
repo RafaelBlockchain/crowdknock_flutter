42d3fb3db649c16ef5eef33f640617b27d7741db
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-12 19:45:55 -0500

    Create MainActivity.kt

---
+++ android/app/src/main/java/com/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.crowdknock  // ← Ajusta este nombre de paquete a tu dominio o nombre real
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
