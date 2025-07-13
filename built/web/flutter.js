08ef0ef53c0d968537b71f0aa4c1f1b4e626450b
Rafael Gonzalez Cortes <rafaeltoronto@yahoo.ca>
2025-07-12 19:34:47 -0500

    Delete built/web directory

--- built/web/flutter.js
+++
@@ -1,9 +0,0 @@
-// web/flutter.js
-
-(function() {
-  let script = document.createElement('script');
-  script.src = 'main.dart.js';
-  script.defer = true;
-  document.body.appendChild(script);
-})();
-
